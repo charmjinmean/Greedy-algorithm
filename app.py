1a400512c2b78f394a75d0f442e0da6c0f42ab88
charmjinmean <charmjinmean@gmail.com>
2025-02-06 16:07:23 +0900

    그리디 알고리즘 클래스,250206 16:00

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+def get_unit_count(amount, won_list): #중첩 함수, #파라미터터
+
+    money = amount
+    won_dict = {}
+    for won in won_list:
+        won_dict[won] = money // won
+        money %= won
+        print(f"{won}원 : {won_dict[won]}개")
+    print("거스름돈", amount)
+    return won_dict
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     print("전송된 데이터 방식")
    
-    won_50000=50000
-    won_10000=10000
-    won_5000=5000
-    won_1000=1000
-    won_500=500
-    won_100=100
-    won_50=50
-    won_5=5
-
     won_list = [50000, 10000, 5000, 1000, 
                 500, 100, 50 ,10]
     
@@ -25,7 +27,7 @@ def index():
     mok = None
     nmg = None 
     money = None
-
+ 
     if request.method == 'POST':
         
         total = request.form.get('total') 
@@ -35,24 +37,15 @@ def index():
         price = int(price)
 
         if price > total :
-           
             error_msg = "음수는 허용되지 않습니다."
         else :
-
+            amount = total - price
             for won in won_list :
                 print(won)
 
-            amount = total - price 
-            money = amount
+            get_unit_count(amount, won_list)
 
-            for won in won_list:
-                mok = money // won
-                money = money % won
-                print(f"{won}원 : {mok}개")
-
-            print("거스름돈", amount)
-
-    return render_template("index.html", won_list=won_list, mok = mok, nmg = nmg, money=money,
+    return render_template("index.html", won_list=won_list, money=money,
                            total=total, price = price, amount=amount, error_msg=error_msg)   
 
 if __name__ == '__main__':
